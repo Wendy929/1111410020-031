ba0b622561b247cff177934cd710fa6fc7da77fd
Wendy929 <134511380+Wendy929@users.noreply.github.com>
2024-04-14 19:20:48 +0800

    Add files via upload

---
+++ 414.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("words =");
+            string str1 = Console.ReadLine();
+   
+           Console.WriteLine("x=");
+
+            if (str1.Length > 50)
+            {
+                Console.WriteLine("請輸入50字以內");
+                System.Environment.Exit(System.Environment.ExitCode);
+            }
+            string[] words = str1.Split(' ');
+
+            char one = Console.ReadLine().ToLower()[0];
+               
+                for (int i = 0; i < words.Length; i++)
+                {
+                    if (words[i].IndexOf(one) >= 0)
+                    {
+                        Console.WriteLine("["+i+"]");
+                    }
+
+                }
+            }
+           
+
+            
+    }
+}
+    
